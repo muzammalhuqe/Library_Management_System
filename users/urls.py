c70ac868dc302a13dda6085b8ef41f6d32a66a27
md muzammal huqe <muzammalraj@gmail.com>
2024-03-14 17:54:42 +0600

    logout function  add

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('singup/', views.SignUpView.as_view(), name = 'singup'),
     path('active/<uid64>/<token>/', views.activate, name = 'activate'),
     path('login/', views.UserLoginView.as_view(), name='login'),
-    path('logout/', views.logout_view.as_view(), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
